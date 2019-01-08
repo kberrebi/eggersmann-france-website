651f579e7ef7d4974c46be6b32ab6c0c3da66fbd
kberrebi <kylian.berrebi@atos.net>
2019-01-08 17:59:13 +0100

    Added first steps to sort the JSON

--- src/app/distributors/distributors.component.ts
+++ src/app/distributors/distributors.component.ts
@@ -11,12 +11,33 @@ import distributors from '../../assets/JSON/distributors.json';
 export class DistributorsComponent implements OnInit {
 
   focus = '';
+  regions = [{formattedName: "IledeFrance", content: []}, {formattedName: "AuvergneRhoneAlpes", content: []}, {formattedName: "BourgogneFrancheComte", content: []},
+             {formattedName: "Bretagne", content: []}, {formattedName: "Corse", content: []}, {formattedName: "Occitanie", content: []}, {formattedName: "HautsdeFrance", content: []},
+             {formattedName: "ProvenceAlpesCoteAzur", content: []}, {formattedName: "NouvelleAquitaine", content: []}, {formattedName: "Switzerland", content: []},
+             {formattedName: "Luxembourg", content: []}, {formattedName: "Maroc", content: []}, {formattedName: "NouvelleCaledonie", content: []}, {formattedName: "Barthelemy", content: []},
+             {formattedName: "GrandEst", content: []}, {formattedName: "CentreValdeLoire", content: []}, {formattedName: "Normandie", content: []}, {formattedName: "PaysdelaLoire", content: []}
+  ]
 
-  constructor(public languagesService: LanguagesService, public navigationService: NavigationService) { }
+  constructor(public languagesService: LanguagesService, public navigationService: NavigationService) {
+    this.sortDistributors();
+  }
 
   ngOnInit() {
-    console.log(distributors);
+    this.regions.forEach(region => {
+      console.log(region);
+    });
     this.navigationService.currentPage = 'distributors';
+  }
+
+  sortDistributors() {
+    console.log('sorting');
+    this.regions.forEach(localRegion => {
+      distributors.regions.forEach(externalRegion => {
+        if (externalRegion.formattedName == localRegion.formattedName) {
+          localRegion.content.push(externalRegion.showrooms);
+        }
+      })
+    });
   }
 
   goToExpansionPanel(region, focused = '') {
